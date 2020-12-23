a15cd7944754d591a44445108d757ca8c9e0b947
Deilan <deilan@users.noreply.github.com>
2020-12-24 02:59:40 +0300

    move some common code to a separate project

---
+++ RavenDB.Issues.RavenDB-16042.Common/ArrangeUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace RavenDB.Issues.RavenDB_16042.Common
+{
+    public static class ArrangeUtils
+    {
+        public static string PrepareDataDirectory()
+        {
+            var ravenDbSourcePath = Path.Combine(FileSystemUtils.GetProjectPath(), "RavenDB");
+            var ravenDbDestinationPath = Path.Combine(AppContext.BaseDirectory, "RavenDB");
+            if (Directory.Exists(ravenDbDestinationPath))
+            {
+                Directory.Delete(ravenDbDestinationPath, true);
+            }
+            FileSystemUtils.CopyDirectory(ravenDbSourcePath, ravenDbDestinationPath);
+            return ravenDbDestinationPath;
+        }
+    }
+}
---
+++ RavenDB.Issues.RavenDB-16042.Common/FileSystemUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RavenDB.Issues.RavenDB_16042.Common
+{
+    public class FileSystemUtils
+    {
+        public static string GetProjectPath()
+        {
+            var exePath = Path.GetDirectoryName(AppContext.BaseDirectory);
+            var appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return appRoot;
+        }
+
+        public static void CopyDirectory(string source, string destination)
+        {
+            foreach (string dirPath in Directory.GetDirectories(source, "*",
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(source, destination));
+
+            foreach (string newPath in Directory.GetFiles(source, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(source, destination), true);
+        }
+    }
+}
